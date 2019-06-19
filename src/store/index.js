103ccb4bcf4cb0fc92bbb68b0fafb6076d353954
danielbecker <dan@jacaranda.tech>
2019-06-19 17:41:38 -0400

    Added blank modules.

--- src/store/index.js
+++ src/store/index.js
@@ -1,19 +1,19 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
-import user from './user'
+import users from './users'
 import shared from './shared'
-import database from './database'
-import table from './table'
-import query from './query'
+import databases from './databases'
+import tables from './tables'
+import queries from './queries'
 
 Vue.use(Vuex)
 
 export const store = new Vuex.Store({
   modules: {
-    user: user,
+    users: users,
     shared: shared,
-    database: database,
-    table: table,
-    query: query
+    databases: databases,
+    tables: tables,
+    queries: queries
   }
 })
---
+++ src/store/worksheets/index.js
@@ -0,0 +1,14 @@
+export default {
+  state: {
+    id: null,
+    title: null,
+    createDate: null,
+    updateDate: null,
+    database: null,
+    table: null,
+    query: null
+  },
+  mutations: {},
+  actions: {},
+  getters: {}
+}
