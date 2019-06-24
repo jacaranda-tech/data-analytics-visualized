17800ab7927bfcc5404370ed49025abd3492ea3d
danielbecker <dan@jacaranda.tech>
2019-06-24 19:19:41 -0400

    Added blank modules.

--- src/store/index.js
+++ src/store/index.js
@@ -2,18 +2,12 @@ import Vue from 'vue'
 import Vuex from 'vuex'
 import users from './users'
 import shared from './shared'
-import databases from './databases'
-import tables from './tables'
-import queries from './queries'
 
 Vue.use(Vuex)
 
 export const store = new Vuex.Store({
   modules: {
     users: users,
-    shared: shared,
-    databases: databases,
-    tables: tables,
-    queries: queries
+    shared: shared
   }
 })
